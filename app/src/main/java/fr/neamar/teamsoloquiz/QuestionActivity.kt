9f2d9ed2180ca5645ccb035e515c2c747b9ad4ab
Neamar <neamar@neamar.fr>
2018-06-28 15:00:14 +0100

    Added progress bar

--- app/src/main/java/fr/neamar/teamsoloquiz/QuestionActivity.kt
+++ app/src/main/java/fr/neamar/teamsoloquiz/QuestionActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.widget.ProgressBar
 import android.widget.TextView
 import fr.neamar.teamsoloquiz.adapter.Question
 import fr.neamar.teamsoloquiz.adapter.SimpleAnswerAdapter
 import kotlinx.android.synthetic.main.activity_question.*
+import java.util.*
 
 class QuestionActivity : AppCompatActivity() {
     val TAG = "QuestionActivity"
@@ -19,16 +21,40 @@ class QuestionActivity : AppCompatActivity() {
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        if (intent == null ||!intent.hasExtra("question")) {
+        if (intent == null || !intent.hasExtra("question")) {
             Log.e(TAG, "No question provided");
         }
 
-        val question : Question = intent.getSerializableExtra("question") as Question;
+        val question: Question = intent.getSerializableExtra("question") as Question;
 
         Log.i(TAG, "Displaying question " + question.question)
 
         findViewById<TextView>(R.id.question).text = question.question;
 
-        findViewById<RecyclerView>(R.id.answers).adapter = SimpleAnswerAdapter(question.answers)
+        findViewById<RecyclerView>(R.id.answers).adapter = SimpleAnswerAdapter(question.answers, this, question.id)
+
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar);
+
+
+        var remainingTime = question.timeout * 1000;
+        progressBar.max = remainingTime;
+
+
+        //Declare the timer
+        val t = Timer()
+        //Set the schedule function and rate
+        t.scheduleAtFixedRate(object : TimerTask() {
+
+            override fun run() {
+                progressBar.progress = remainingTime;
+                remainingTime -= 100;
+            }
+
+        },
+                //Set how long before to start calling the TimerTask (in milliseconds)
+                0,
+                //Set the amount of time between each execution (in milliseconds)
+                100)
+
     }
 }
--- app/src/main/java/fr/neamar/teamsoloquiz/adapter/SimpleAnswerAdapter.java
+++ app/src/main/java/fr/neamar/teamsoloquiz/adapter/SimpleAnswerAdapter.java
@@ -1,22 +1,40 @@
 package fr.neamar.teamsoloquiz.adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import org.json.JSONException;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.iid.FirebaseInstanceId;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import fr.neamar.teamsoloquiz.R;
 
 public class SimpleAnswerAdapter extends RecyclerView.Adapter<SimpleAnswerHolder> {
+    private static final String TAG = "SimpleAnswerAdapter";
     private ArrayList<String> answers;
     private int selectedItem = -1;
+    private int questionId;
 
-    public SimpleAnswerAdapter(ArrayList<String> answers) throws JSONException {
+    private Context context;
+
+    public SimpleAnswerAdapter(ArrayList<String> answers, Context context, int questionId) throws JSONException {
+        this.context = context;
+        this.questionId = questionId;
         this.answers = answers;
         updateAnswers(answers);
     }
@@ -52,11 +70,52 @@ public class SimpleAnswerAdapter extends RecyclerView.Adapter<SimpleAnswerHolder
     }
 
     public void setSelectedItem(int position) {
-        selectedItem = position;
-        notifyDataSetChanged();
+        if(selectedItem == -1) {
+            selectedItem = position;
+            sendAnswerToServer();
+            notifyDataSetChanged();
+        }
     }
 
     public int getSelectedItem() {
         return selectedItem;
+    }
+
+    private void sendAnswerToServer() {
+        // Instantiate the RequestQueue.
+        final RequestQueue queue = Volley.newRequestQueue(context);
+
+        try {
+            JSONObject payload = new JSONObject();
+            payload.put("answer", selectedItem);
+            payload.put("id", FirebaseInstanceId.getInstance().getToken());
+            JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.POST, context.getString(R.string.API_URL) + "/questions/" + questionId,
+                    payload,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.i(TAG, "Answer sent to server for question " + questionId);
+                            queue.stop();
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, error.toString());
+
+                    try {
+                        String responseBody = new String(error.networkResponse.data, "utf-8");
+                        Log.i(TAG, responseBody);
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    } catch (NullPointerException e) {
+                    }
+                }
+            });
+
+            queue.add(jsonRequest);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
